1f6006ab6a9674d2a00e0fdd8b677d30255a729a
Your Name <you@example.com>
2018-08-31 15:05:28 +0800

    commit

---
+++ src/main/java/RandomNum/RandNum/main.java
@@ -0,0 +1,14 @@
+package RandomNum.RandNum;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class main {
+
+	public static void main(String[] args) {
+		SpringApplication.run(main.class, args);
+           
+	}
+	
+}
+
